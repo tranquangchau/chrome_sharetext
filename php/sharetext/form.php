1ca7ac6a52be72f50967444d9a771309bb02c95b
Trần Quang Châu <tranquangchau155@gmail.com>
2016-12-27 20:35:26 +0700

    Update form.php

--- php/sharetext/form.php
+++ php/sharetext/form.php
@@ -1,11 +1,22 @@
+
+
 Welcome form
-<form method="post" action="<?= SITE_URL;?>">
-	<input type="text" name="filename"/ style="width: 100%;">
-	<textarea name="textdata" style="width: 100%;"></textarea>
+<form id="myForm" method="post" action="<?= SITE_URL;?>">
+	<input type="text" id="te1" name="filename"/ style="width: 100%;">
+	<textarea id="textdata" name="textdata" style="width: 100%;"></textarea>
 	<input type="submit" value="save"/>
     </form>
-	
+    <br/>
+	<button onclick="add()">add favorit</button><br/>
 	<a href="<?=SITE_URL?>show.php">view all post</a>
 	<br/>
 	<a href="<?=SITE_URL?>?action=logout">Logout</a>
-	
+
+<script>
+function add(){
+document.getElementById("te1").value='favorist';
+document.getElementById("textdata").value='<?= $_GET['title']." ".$_GET['link'] ?>';
+document.getElementById("myForm").submit();
+
+}
+</script>
