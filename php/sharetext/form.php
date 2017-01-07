d145a4d0beac5625456137606d94312420832c0b
Trần Quang Châu <tranquangchau155@gmail.com>
2017-01-08 01:27:57 +0700

    Update form.php

--- php/sharetext/form.php
+++ php/sharetext/form.php
@@ -1,22 +1,43 @@
 
 
 Welcome form
+<br><button onclick="plus_text(1)">test</button>
 <form id="myForm" method="post" action="<?= SITE_URL;?>">
 	<input type="text" id="te1" name="filename"/ style="width: 100%;">
 	<textarea id="textdata" name="textdata" style="width: 100%;"></textarea>
-	<input type="submit" value="save"/>
+	<input type="submit" value="save"/> 
+	
     </form>
-    <br/>
-	<button onclick="add()">add favorit</button><br/>
+    <button onclick="add(1)">add1</button>	
+    <button onclick="add(2)">add cpan</button><br/>
+    <input type="hidden" id="a1" value="0">
+    <input type="hidden" id="a2" value="0">
 	<a href="<?=SITE_URL?>show.php">view all post</a>
 	<br/>
 	<a href="<?=SITE_URL?>?action=logout">Logout</a>
 
 <script>
-function add(){
-document.getElementById("te1").value='favorist';
-document.getElementById("textdata").value='<?= $_GET['title']." ".$_GET['link'] ?>';
-document.getElementById("myForm").submit();
+function add(id){
+	var a = document.getElementById("a"+id).value;
+	if(a==0){
+		document.getElementById("a"+id).value="1";
+		
+		
+		if(id==1){
+			document.getElementById("te1").value='favorist';
+		}else{
+			document.getElementById("te1").value='favorist cty';
+		}
+		document.getElementById("textdata").value='<?= $_GET['title']." ".$_GET['link'] ?>';
+	}else{
+		//alert("subit");		
+		document.getElementById("myForm").submit();
+	}
 
+}
+function plus_text(id){
+	if(id==1){
+		document.getElementById("textdata").value='<i classs="testok">Testok<i>'+document.getElementById("textdata").value;
+	}
 }
 </script>
